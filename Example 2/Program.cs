22074c63be9d11214e3f30663f32d752ade32610
CmdWilson <141258229+CmdWilson@users.noreply.github.com>
2023-09-12 23:12:58 +0300

    Add files via upload

---
+++ Example 2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y1 = k1 * x + b1;
+double y2 = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {y1}, Y: {y2}");
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+    }
+}
+
